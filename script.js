f56fd0ef246ea13220ce68f6c223a11bfb4f5efc
Om Thakare <omthakare1606@gmail.com>
2022-10-12 16:44:28 +0530

    created skeleton js and html

--- script.js
+++ script.js
@@ -7,52 +7,111 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return 2;
-  } else if (computerSelection == "rock" && playerSelection == "paper") {
+  } else if (computerSelection === "rock" && playerSelection === "paper") {
     return 1;
-  } else if ((computerSelection = "rock" && playerSelection == "scissors")) {
+  } else if (computerSelection === "rock" && playerSelection === "scissors") {
     return 0;
-  } else if ((computerSelection = "paper" && playerSelection == "rock")) {
+  } else if (computerSelection === "paper" && playerSelection === "rock") {
     return 0;
-  } else if ((computerSelection = "paper" && playerSelection == "scissors")) {
+  } else if (computerSelection === "paper" && playerSelection === "scissors") {
     return 1;
-  }else if ((computerSelection = "scissors" && playerSelection == "rock")) {
+  } else if (computerSelection === "scissors" && playerSelection === "rock") {
     return 1;
-  }else if ((computerSelection = "scissors" && playerSelection == "paper")) {
+  } else if (computerSelection == "scissors" && playerSelection === "paper") {
     return 0;
+  } else {
+    return "nothing";
   }
 }
+const buttons = document.querySelectorAll("button");
 
-function game() {
-  var com = 0;
-  var playr = 0;
-  
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("What's your sign?");
+// buttons.forEach((button) => {
+//   // and for each one we add a 'click' listener
+//   button.addEventListener("click", () => {
+//     let comp = getComputerChoice();
+//     let playerSelection = button.innerText;
+//     game(playerSelection);
+
+//   });
+// });
+
+let k = 0;
+const content = document.querySelector("div");
+
+var com = 0;
+var playr = 0;
+const status = document.createElement("div");
+const result = document.createElement("div");
+const score = document.createElement("div");
+score.classList.add("scr");
+score.textContent = com + " " + playr;
+// content.insertBefore(score, buttons[0]);
+content.appendChild(score);
+
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    let playerSelection = button.innerText;
+    if (playr == 5 || com == 5) {
+      result.textContent = "";
+      k = 0;
+      com = 0;
+      playr = 0;
+      score.textContent = com + " " + playr;
+    }
+    k = k + 1;
+
     let computerSelection = getComputerChoice();
-    if(playRound(playerSelection,computerSelection)==1){
-      playr=playr+1;
+    if (playRound(playerSelection, computerSelection) == 1) {
+      playr = playr + 1;
+      status.textContent = "";
+    } else if (playRound(playerSelection, computerSelection) == 2) {
+      status.textContent = "Tie";
+      content.appendChild(status);
+    } else {
+      com = com + 1;
+      status.textContent = "";
     }
-    else if(playRound(playerSelection,computerSelection)==2){
-      console.log("tie")
+    score.textContent = com + " " + playr;
+    // content.insertBefore(score, buttons[0]);
+    content.appendChild(score);
+    
+    console.log(com, playr);
+    if (playr == 5 || com == 5) {
+      if (playr == com) {
+        // console.log("it's a tie game");
+        result.textContent = "It's a tie game";
+      } else if (playr > com) {
+        // console.log("you WIN");
+        result.textContent = "YOU WIN";
+      } else {
+        // console.log("you lose better luck next time");
+        result.textContent = "you lose better luck next time";
+      }
+      content.appendChild(result);
     }
-    else{
-      com=com+1;
-    }
-    console.log(playr,com)
-  }
-  if(playr==com){
-    console.log("it's a tie game")
-  }
-  else if(playr>com){
-    console.log("you WIN")
+  });
+});
 
-  }
-  else{
-    console.log("you lose better luck next time")
-  }
+const rstbutn = document.getElementById("rst");
+rstbutn.addEventListener("click", reset);
 
+function reset() {
+  com = 0;
+  playr = 0;
+  k = 0;
+  score.textContent =  com + " " + playr;
+  result.textContent = ""; 
 }
-
-
-game();
-
+// for (let i = 0; i < 5; i++) {
+//   let computerSelection = getComputerChoice();
+//   if (playRound(playerSelection, computerSelection) == 1) {
+//     playr = playr + 1;
+//   } else if (playRound(playerSelection, computerSelection) == 2) {
+//     status.textContent = "Tie";
+//     content.appendChild(status);
+//   } else {
+//     com = com + 1;
+//   }
+//   console.log(playr, com);
+// }
